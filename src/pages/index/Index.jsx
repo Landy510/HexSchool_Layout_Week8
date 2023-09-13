03d1bf25befb0d9e29009c2bba25db1319e0f4af
Landy510 <randy.chen@tpisoftware.com>
2023-09-13 09:39:42 +0800

    feat: the layout of index page 1. add - the layout of banner section

--- src/pages/index/Index.jsx
+++ src/pages/index/Index.jsx
@@ -1,12 +1,14 @@
 import styles from './index.module.scss';
 import carouselImg from '@/assets/images/carousel.png';
+import line_end_arrow_defaultImg from '@/assets/images/line_end_arrow_default.png';
+import line_start_arrow_hoverImg from '@/assets/images/line_start_arrow_hover.png';
 
 const Index = () => {
   return (
     <div>
       <div className={`${styles['gradient-bg']}`}>
-        <div className="container-fluid 12312">
-          <div className="row 123123">
+        <div className="container-xxl px-5">
+          <div className="row">
             <div className="col-3">
               <p className='py-2 mt-auto mb-11 text-white border-[1px] border-solid border-t-0 border-l-0 border-r-0 border-white font-bold'>最新消息！新版本 v15.4 釋出，搶先體驗！</p>
               <img 
@@ -50,16 +52,36 @@ const Index = () => {
         
       </div>
       <div className={`${styles['img-bg']} fs-48 text-white font-bold tracking-[3rem]`}>
-        <p className='mb-[12px]'>什麼是</p>
-        <p className='mb-3'>ALPHABOX+</p>
-        <div className="bg-white h-[1px]"></div>
-        <div>
-          <img 
-            src={carouselImg} 
-            alt="" 
-            className='max-w-[1296px]'
-          />
+        <div className='px-5'>
+          <div className='bg-gradient-to-r from-primary-500 to-primary-300 h-[40px]'></div>
+          <p className='mb-[12px]'>什麼是</p>
+          <p className='mb-3'>ALPHABOX+</p>
         </div>
+        <div className="bg-white h-[1px]"></div>
+        <div className='flex items-center justify-center py-6 px-5'>
+          <div>
+            <img 
+              src={line_start_arrow_hoverImg} 
+              alt="" 
+              className='w-full max-w-[180px]'
+            />
+          </div>
+          <div className='mx-[52px]'>
+            <img 
+              src={carouselImg} 
+              alt="" 
+              className='max-w-[1296px] w-full'
+            />
+          </div>
+          <div>
+            <img 
+              src={line_end_arrow_defaultImg} 
+              alt="" 
+              className='w-full max-w-[180px]'
+            />
+          </div>
+        </div>
+        <div className='bg-gradient-to-r from-primary-500 to-primary-300 h-[40px] mx-5'></div>
       </div>
     </div>
   )
