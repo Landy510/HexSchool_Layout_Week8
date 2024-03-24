7c3daef53e61a58aa1099ef2d0ea899bcb6677a1
Landy510 <randy.chen@tpisoftware.com>
2024-03-24 12:30:03 +0800

    build: removed unnecessary tailwindCSS settings

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,9 +3,7 @@ export default {
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
-    './src/**/**/*.{html,jsx,tsx}',
-    './src/**/**/**/*.{html,jsx,tsx}',
-    './src/**/**/**/**/*.{html,jsx,tsx}',
+    './src/**/*.{html,jsx,tsx}'
   ],
   theme: {
     container: {
